7311b1b19ff90a685c226c23036c7f5be7dfbc71
Polina <deadvinny1@gmail.com>
2023-07-09 13:42:22 +0300

    Добавлен пример библиотеки массива

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col) // воид - функция, не возвращаюшая значение
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+
+}
+
+int[] array = new int[10];
+
+FillArray(array); //заполнил массив
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array); // распечатал массив
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
